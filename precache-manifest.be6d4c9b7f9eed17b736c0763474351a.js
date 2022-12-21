046f0d9e8b3dcf5fc67191682164fd9ae3826af2
dhirajpatil2346 <dhirajpatil2346>
2022-12-21 14:24:15 +0530

    Updates

---
+++ precache-manifest.be6d4c9b7f9eed17b736c0763474351a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "23f74c613eec031cb3ca",
+    "url": "/TextUtilsReact/static/css/main.4690f505.chunk.css"
+  },
+  {
+    "revision": "23f74c613eec031cb3ca",
+    "url": "/TextUtilsReact/static/js/main.23f74c61.chunk.js"
+  },
+  {
+    "revision": "06770e5b67b75f5dcad1",
+    "url": "/TextUtilsReact/static/js/1.06770e5b.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextUtilsReact/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "3d69f6f974866449936d",
+    "url": "/TextUtilsReact/static/js/runtime~main.3d69f6f9.js"
+  },
+  {
+    "revision": "d60a6af6fc6088a86c71e3821bb19dc2",
+    "url": "/TextUtilsReact/index.html"
+  }
+];
